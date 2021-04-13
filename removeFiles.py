96fb2e3739dd777da43a74558f63017ec545546e
AryanSteve1118 <72647556+AryanSteve1118@users.noreply.github.com>
2021-04-13 09:26:11 +0530

    Update removeFiles.py

--- removeFiles.py
+++ removeFiles.py
@@ -8,7 +8,7 @@ def main():
     path="F:/whitehatjr files/classes/my"
     days=30
     seconds=time.time()-(days*24*60*60)
-    if os.path.exists() :
+    if os.path.exists(path) :
         for root_folder, folders, files in os.walk(path):
             if seconds >= get_file_or_folder_age(root_folder):
                 remove_folder(root_folder)
